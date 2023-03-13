b418e1235dd9c3d3f646ef340346c5969287c3e8
AlirezaMohyyaldin <126543376+AlirezaMohyyaldin@users.noreply.github.com>
2023-03-13 19:59:50 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_alireza
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("enter your number you want caculate:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter your + - / * :");
+            char s = Convert.ToChar(Console.ReadLine());
+            if(s == '+')
+            {
+                Console.WriteLine(y + x);
+            }
+            if(s == '-')
+            {
+                    Console.WriteLine(x - y);
+            }
+            if(s == '*')
+            {
+                Console.WriteLine(x * y);
+            }
+            if(s == '/')
+            {
+                Console.WriteLine(x / y);
+            }
+             
+            
+
+        }
+    }
+}
